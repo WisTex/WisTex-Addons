68e0c541216c716217986411f099261902eea4a7
Scott M. Stolz <sstolz@wistex.com>
2024-01-15 20:12:28 -0600

    Update Functions

--- neuhub_navbar_var/neuhub_navbar_var.php
+++ neuhub_navbar_var/neuhub_navbar_var.php
@@ -26,16 +26,16 @@ use Zotlabs\Extend\Route;
 
 /**
  * * This function registers (adds) the hook handler and admin route.
- * The testplugin2_nav() hook handler is registered for the "nav" hook
- * The "testplugin2" route is created for Mod_Testplugin2 module 
+ * The neuhub_navbar_var_nav() hook handler is registered for the "nav" hook
+ * The "neuhub_navbar_var" route is created for Mod_Neuhub_navbar_var module 
 */
-function testplugin2_load() {
+function neuhub_navbar_var_load() {
 	Hook::register('nav', 'addon/neuhub_navbar_var/neuhub_navbar_var.php', 'neuhub_navbar_var_nav');
 	Route::register('addon/neuhub_navbar_var/Mod_Neuhub_navbar_var.php', 'neuhub_navbar_var');
 }
 
 // * This function unregisters (removes) the hook handler and admin route.
-function testplugin2_unload() {
+function neuhub_navbar_var_unload() {
 	Hook::unregister('nav', 'addon/neuhub_navbar_var/neuhub_navbar_var.php', 'neuhub_navbar_var_nav');
 	Route::unregister('addon/neuhub_navbar_var/Mod_Neuhub_navbar_var.php', 'neuhub_navbar_var');
 }
@@ -44,14 +44,14 @@ function testplugin2_unload() {
  * * This function runs when the hook handler is executed.
  * @param $templateObj: A reference to the corresponding template object in the global scope
 */
-function testplugin2_nav(&$templateObj) {
+function neuhub_navbar_var_nav(&$templateObj) {
 	$current_theme = current(Zotlabs\Render\Theme::current());
 	
-	// Reload the "config" (database table) settings in the "testplugin2" category, because they not available yet
+	// Reload the "config" (database table) settings in the "neuhub_navbar_var" category, because they not available yet
 	unset(App::$config[$current_theme]);
 	load_config($current_theme);
 
-	// Load "testplugin2" settings (custom variables) into the template object
+	// Load "tneuhub_navbar_var" settings (custom variables) into the template object
 	if (!empty(App::$config[$current_theme])) {
 		foreach (App::$config[$current_theme] as $varName => $varValue) {
 			if ($varName != 'config_loaded') {
